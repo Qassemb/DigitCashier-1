f16eb9b81ae94d5a23b87ccae825e861f09df3a8
Qassemb <abojoue@gmail.com>
2016-11-30 11:22:53 +0100

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -5,30 +5,32 @@ public class Login {
 		public static void loginAccess()
 		
 		Scanner scan = new Scanner(System.in);
-		int loginCode;					//Deklarerar variabeln som anv‰ndaren anger nedan.
-		int[] loginDB = {201,202,203,302,503}; //Anst‰llda med olika lˆsen och behˆrigheter. HÂrdkodad array DB.
+		int loginCode;					//Deklarerar variabeln som anv√§ndaren anger nedan.
+		int[] loginDB = {201,202,203,302,503}; //Anst√§llda med olika l√∂sen och beh√∂righeter. H√•rdkodad array DB.
 		
-		String[] position = {"", "", "Kassa", "System admin", "", "Fˆrs‰ljningschef" };
+		String[] position = {"", "", "Kassa", "System admin", "", "F√∂rs√§ljningschef" };
 		int i = 0;
 		int j = 0;
 		
-	//	System.out.println("V‰lkommen!");
-	//	System.out.print("Ange ditt lˆsenord: ");
-	//	loginCode = scan.nextInt();//l‰ser in angivet lˆsenord
+	//	System.out.println("V√§lkommen!");
+	//	System.out.print("Ange ditt l√∂senord: ");
+	//	loginCode = scan.nextInt();//l√§ser in angivet l√∂senord
 				
-		while ( i<loginDB.length )                  //Kontroll om lˆsenord existerar. Loopar genom vektorn.
+		while ( i<loginDB.length )                  //Kontroll om l√∂senord existerar. Loopar genom vektorn.
 		{
-			if ( loginCode == loginDB [i]  ) 		//Om koden st‰mmer sÂ sparas i som j.
+			if ( loginCode == loginDB [i]  ) 		//Om koden st√§mmer s√• sparas i som j.
 			{	
-				int accessNo = loginCode / 100;		//kontrollera fˆrsta siffran
-				System.out.println("Du har behˆrighet till: " + position[accessNo]);
+				int accessNo = loginCode / 100;		//kontrollera f√∂rsta siffran
+				System.out.println("Du har beh√∂righet till: " + position[accessNo]);
 				return;
 			}
 		i++;
 		
 		}
-		if (i>j) {								//Om inte koden st‰mmer, dvs loopen har fullfˆljts utan att hitta r‰tt kod, sÂ skrivs felmeddelandet ut.
+		if (i>j) {								//Om inte koden st√§mmer, dvs loopen har fullf√∂ljts utan att hitta r√§tt kod, s√• skrivs felmeddelandet ut.
 			System.out.println("fel kod");
 			}
 					
 }//end class
+
+// Just a test
